1338c31c3aee01b8ff4ef65d0633e268c857b013
sharanaru <55450674+sharanaru@users.noreply.github.com>
2019-10-03 15:04:15 -0600

    Update test.c

--- Project2/test.c
+++ Project2/test.c
@@ -1,4 +1,3 @@
-
 /*
  * Team: Abhijeet Dutt Srivastava and Sharan Arumugam
  * PES Project 2
@@ -14,7 +13,7 @@
 
 /*NOTE ! PCR - PC_Run PCD - PC_Debug FBR - FB_Run FBD - FB_Debug*/
 
-#if defined PCR || defined PCD
+#if defined PCR || defined PCD //https://stackoverflow.com/questions/2998864/how-to-add-a-or-condition-in-ifdef
  #include<stdio.h>
  #include<stdint.h>
  #include<time.h>
@@ -28,28 +27,28 @@
 #include "clock_config.h"
 #include "MKL25Z4.h"
 #include "fsl_debug_console.h"
-
+#include "led.h"
 // Delay function
 void delay(uint32_t timer)
 {
-   for(uint32_t i=0;i<timer;i++)
+   for(uint32_t i=0;i<timer;i++) //holds execution till delay is achieved
    { __asm("NOP");
     }
 }
-const uint32_t scaler = 3000;
+const uint32_t scaler = 3000; //adjust for timing value;
 #endif
+
 
 #if defined PCR || defined PCD
 void LED_RUN(int x)
 {
 if(x<=5||x==18||x==19)
-		printf("LED BLUE ");
+        printf("LED BLUE ");
 if((x>5)&&(x<12))
-	printf("LED RED ");;
+    printf("LED RED ");;
 if((x>11)&&(x<18))
-	printf("LED GREEN ");
+    printf("LED GREEN ");
 }
-
 void LED_STATUS(int x)
 {
     if(x%2==0)
@@ -59,77 +58,24 @@ void LED_STATUS(int x)
 }
 #endif
 
-
-
-#ifdef FBR
-
-/*
- * @brief   Application entry point.
- */
-// LED blinking for FB_RUN
-void LED_RUN_FBR(int x)
-{
-
-if(x<=5||x==18||x==19)
-LED_BLUE_TOGGLE();
-
-if((x>5)&&(x<12))
-	LED_RED_TOGGLE();
-if((x>11)&&(x<18))
-	LED_GREEN_TOGGLE();
-}
-#endif
-
-#ifdef FBD
-// LED run function for FB_debug with printfs
-void LED_RUN_FBD(int x)
-{
-
-if(x<=5||x==18||x==19)
-{
-	LED_BLUE_TOGGLE();printf("LED BLUE");
-}
-
-if((x>5)&&(x<12))
-	{
-	LED_RED_TOGGLE();printf("LED RED");
-	}
-if((x>11)&&(x<18))
-{
-LED_GREEN_TOGGLE();
-printf("LED GREEN");
-}
-}
-void LED_STATUS_FBD(int x)
-{
-    if(x%2==0)
-        printf(" ON ");
-    else
-        printf(" OFF ");
-}
-#endif
-
 int main(void)
 {
 // Common for both FB_debug and FB_run
 #if defined FBR || defined FBD
-/* Board pin, clock, debug console init */
+/* Board pin, clock, debug console initialisation */
     BOARD_InitPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
     /* Delay lookup table */
     uint32_t timing[]={3000,1000,2000,600,1000,400,1000,200,500,100,500,100,500,100,1000,200,1000,400,2000,600};
-LED_BLUE_INIT(1);LED_RED_INIT(1);LED_GREEN_INIT(1);
+    LED_BLUE_INIT(1);LED_RED_INIT(1);LED_GREEN_INIT(1);
 #endif
 
 while (1)
     {
     	for(int i=0;i<20;i++)
     	{
-#if defined PCR || defined PCD
-    		time_t Timestamp;
-    		uint32_t timing[]={3000,1000,2000,600,1000,400,1000,200,500,100,500,100,500,100,1000,200,1000,400,2000,600};uint32_t TimeCheck=clock();
-#endif
+
 
 #ifdef FBR
     	LED_RUN_FBR(i);
@@ -144,6 +90,10 @@ while (1)
 #endif
 		// GPIO_TogglePinsOutput(BOARD_LED_GPIO, 1u << BOARD_LED_GPIO_PIN);
 #if defined PCR || defined PCD
+    		time_t Timestamp; time(&Timestamp);
+    		uint32_t timing[]={3000,1000,2000,600,1000,400,1000,200,500,100,500,100,500,100,1000,200,1000,400,2000,600};//contains timings
+    		uint32_t TimeCheck=clock();//stores clock time at start of routine
+
 		        LED_RUN(i);
 				LED_STATUS(i);
 				while((clock()-TimeCheck)<(timing[i]))
@@ -155,13 +105,14 @@ while (1)
 #ifdef PCD
 				printf("%ld \n",clock()-TimeCheck);//for debug
 #endif
-
-#ifdef PCD
+#if defined PCR || defined PCD
 				TimeCheck=clock();
-		        time(&Timestamp);//for debug
+#endif
+#ifdef PCD
+
+				 time(&Timestamp);//for debug
 		        printf("%s\n",ctime(&Timestamp));//for debug
 #endif
         }
-
 }
 }
