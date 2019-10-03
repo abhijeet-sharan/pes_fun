8d5cab93385e50c1cc8cc0fed3ab7fee16c625b1
sharanaru <55450674+sharanaru@users.noreply.github.com>
2019-10-03 15:31:12 -0600

    Add files via upload

---
+++ Project2/withoutmake/board/led.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<board.h>
+void LED_RUN_FBR(int x);// toggles led through predetermined pattern
+// LED run function for FB_debug with printfs
+void LED_RUN_FBD(int x);
+void LED_STATUS_FBD(int x);
